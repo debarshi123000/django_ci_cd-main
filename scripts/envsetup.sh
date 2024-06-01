be35d03d633065b28536a9ec4043e815da959ec8
debarshi123000 <piklud@gmail.com>
2024-06-02 00:09:46 +0530

    Add initial files

---
+++ scripts/envsetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
